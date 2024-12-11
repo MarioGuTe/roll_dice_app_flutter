5125ce96e8e51f65681eef6f8e02d441cb6f7a25
MarioGuTe <mariogutierreztello@gmail.com>
2024-12-11 15:49:15 -0300

    add dice roller stateful widget

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app_flutter/dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.colors, {super.key});
 
   final List<Color> colors;
-
-  void rollDice() {}
 
   @override
   Widget build(context) {
@@ -17,27 +16,7 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomRight,
         ),
       ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/dice-1.png',
-              width: 200,
-            ),
-            TextButton(
-              onPressed: rollDice,
-              style: TextButton.styleFrom(
-                  padding: const EdgeInsets.only(
-                    top: 20,
-                  ),
-                  foregroundColor: Colors.white,
-                  textStyle: const TextStyle(fontSize: 28)),
-              child: const Text('Roll Dice'),
-            )
-          ],
-        ),
-      ),
+      child: const Center(child: DiceRoller()),
     );
   }
 }
